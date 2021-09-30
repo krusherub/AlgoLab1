817a1da951e915bd8a33ed02683fd5668efb26be
krusherub <52534389+krusherub@users.noreply.github.com>
2021-09-30 17:35:00 +0300

    Add files via upload

---
+++ kirill/AlgoritmsLab1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace AlgoritmsLab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+            board.PrintTable();
+
+            Solver solver = new Solver();
+            solver.DepthLimitedSearch(board,3);
+        }
+    }
+}
---
+++ kirill/AlgoritmsLab1/Solver.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Xml;
+
+namespace AlgoritmsLab1
+{
+    public class Solver
+    {
+        public int states = 0;
+        public int iterations = 0;
+        public int deadCorner = 0;
+        public static int MaxDepth = 10;
+
+        
+        public Board RecursiveBestFirstSearch(Board initialBoard)
+        {
+            initialBoard.Collisions = F1(initialBoard);
+            
+            Console.WriteLine(initialBoard.Collisions);
+            var result = RBFS(initialBoard, int.MaxValue) ;
+            if (result is null)
+            {
+                throw new Exception();
+            }
+            result.PrintTable();
+            return result;
+        }
+        
+        private Queue<Board> lastTenBoards = new Queue<Board>();
+        private bool CheckOverFlow(Board board)
+        {
+            foreach (var bb in lastTenBoards)
+            {
+                if (bb.Equals(board) )
+                {
+                    return true;
+                }
+            }
+
+            lastTenBoards.Enqueue(board);
+               
+            if (lastTenBoards.Count == 50)
+            {
+                lastTenBoards.Dequeue();
+            }
+            return false;
+        }
+
+        private Board prev = new Board() {_board = new ChessFigure[8,8]};
+        private Board RBFS(Board board, int limitF)
+        {
+            iterations++;
+            if (board.Collisions == 0)
+            {
+                return board;
+            }
+
+            if (CheckOverFlow(board))
+            {
+                deadCorner++;
+                board.Collisions += 3;
+                return board;
+            }
+            
+            // board.PrintTable();
+            
+            var queue = new Queue<Board>(GetNewBoards(board).OrderBy(x => x.Collisions));
+            while (queue.Any())
+            {
+                var newBoard = queue.Peek();
+ 
+                if (newBoard.Collisions > limitF)
+                {
+                    return board;
+                }
+                
+                prev = newBoard;
+                
+                var dumpBoard =  RBFS(newBoard, newBoard.Collisions);
+                
+                if (dumpBoard.Collisions == 0)
+                {
+                    return dumpBoard;
+                }
+                newBoard.Collisions = dumpBoard.Collisions;
+                queue = new Queue<Board>(queue.OrderBy(x => x.Collisions).ToArray());
+            }
+            return null;
+        }
+           private bool Check(Board.Direction direction, int stepSize, int initialPositionX, int initialPositionY)
+        {
+            if (direction is Board.Direction.Up)
+            {
+                if (initialPositionY - stepSize  < 0)
+                {
+                    return false;
+                }
+
+                if (initialPositionY == 0)
+                {
+                    return false;
+                }
+            }
+            if (direction is Board.Direction.Down)
+            {
+                if (initialPositionY + stepSize  > 7)
+                {
+                    return false;
+                }
+
+                if (initialPositionY == 7)
+                {
+                    return false;
+                }
+            }
+            
+            if (direction is Board.Direction.Left)
+            {
+                if (initialPositionX == 0)
+                    return false;
+                if (initialPositionX - stepSize < 0)
+                    return false;
+            }
+            
+            if (direction is Board.Direction.Right)
+            {
+                if (initialPositionX == 7)
+                    return false;
+                if (initialPositionX + stepSize > 7)
+                    return false;
+            }
+
+          
+            if (direction is Board.Direction.UpLeft)
+            {
+                if (initialPositionX == 0 || initialPositionY==0)
+                    return false;
+                if (initialPositionX - stepSize < 0 || initialPositionY - stepSize < 0 )
+                    return false;
+            }
+
+            if (direction is Board.Direction.DownLeft)
+            {
+                if (initialPositionX == 0 || initialPositionY==7)
+                    return false;
+                if (initialPositionX - stepSize < 0 || initialPositionY + stepSize > 7 )
+                    return false;
+            }
+            
+            if (direction is Board.Direction.UpRight)
+            {
+                if (initialPositionX == 7)
+                    return false;
+                if (initialPositionY == 0)
+                    return false;
+                if (initialPositionX + stepSize > 7)
+                    return false;
+                if (initialPositionY - stepSize < 0)
+                    return false;
+            }
+            
+            if (direction is Board.Direction.DownRight)
+            {
+                if (initialPositionX == 7)
+                    return false;
+                if (initialPositionY == 7)
+                    return false;
+                if (initialPositionX + stepSize > 7)
+                    return false;
+                if (initialPositionY + stepSize > 7)
+                    return false;
+            }
+            return true;
+        }
+           private int F1(Board board)
+        {
+            var collisions = 0;
+            foreach (var queen in board._board)
+            {
+                if (queen==null)
+                {
+                    continue;
+                }
+                for (int y = 0; y < 8; y++)
+                {
+                    for (int x = 0; x < 8; x++)
+                    {
+                        if (board._board[y,x] == queen)
+                        {
+                            for (int option = 0; option < 8; option++)
+                            {
+                                for (var i = 1; i < 8; i++)
+                                {
+                                    if (collisions == 5 && x == 0 && y== 3 && option == 4)
+                                    {
+                                                    
+                                    }
+                                    var newBoard = new Board(board._board);
+
+                                    if (Check((Board.Direction) option,i,x,y))
+                                    {
+                                        if(!newBoard.moveFigure((Queen)queen,(Board.Direction)option,i))
+                                        {
+                                            collisions++;
+                                            break;
+                                        }
+                                    }
+                            
+                                }
+                                //ОБНОВЛЯТЬ МАССИВ КВИН
+                            }
+                            //  break; // наверх
+                            
+                        }
+                    }
+                }
+            }
+            return collisions/2;
+        }
+
+
+         public Board DepthLimitedSearch(Board board, int maxDepth)
+         {
+             stopWatch.Start();
+             var result =  RecursiveDls(board, maxDepth);
+             if (result is Cutoff)
+             {
+                 
+             }
+
+             if (result is Solved)
+             {
+                 Solved sl = (Solved) result;
+                 return sl.Board;
+             }
+             return null ;
+         }
+
+         class Cutoff
+         {
+             
+         }
+         class Failure
+         {
+             
+         }
+
+         class Solved
+         {
+             public Board Board { get; set; }
+
+             public Solved(Board board)
+             {
+                 Board = board;
+             }
+         }
+        private Stopwatch stopWatch = new Stopwatch();
+        private object RecursiveDls( Board board, int maxDepth)
+         {
+            iterations++;
+             if (board.IsRight() )
+                {
+                 Console.WriteLine();
+                 board.PrintTable();
+                stopWatch.Stop();
+                TimeSpan ts = stopWatch.Elapsed;
+                Console.WriteLine();
+                string elapsedTime = String.Format("{0:00}:{1:00}.{2:00}",
+                     ts.Minutes, ts.Seconds,
+                    ts.Milliseconds / 10);
+                Console.WriteLine(elapsedTime, "RunTime");
+
+                Console.WriteLine("iterations:"+ iterations);
+                Console.WriteLine("states:" + states);
+
+                Environment.Exit(1);
+                 return new Solved(board);
+             }
+             
+             if ( maxDepth == 0 )
+             {
+                 return new Cutoff();
+             }
+
+             foreach (var newBoard in GetNewBoards(board))
+             {
+                  RecursiveDls(newBoard, maxDepth - 1);
+             }
+
+             return new Failure();
+         }
+
+         private List<Board> GetNewBoards(Board board)
+         {
+             var list = new List<Board>();
+             foreach (var queen in board._board)
+             {
+                 if (queen == null)
+                 {
+                     continue;
+                 }
+                 for (var i = 1; i < 8; i++)
+                 {
+                     for (int option = 0; option < 8; option++)
+                     {
+                         var newBoard = new Board(board._board);
+                         // newBoard.PrintTable();
+                         newBoard.moveFigure((Queen) queen, (Board.Direction) option, i);
+                         // Console.WriteLine((Board.Direction) option);
+                         // newBoard.PrintTable();
+                         // Console.WriteLine("11111111111111111111111111111");
+                         newBoard.Collisions = F1(newBoard);
+
+                        // if (!newBoard.Contains(5))
+                        // {
+                        //     Console.WriteLine( ((Queen) queen).number + "qqqqqq");
+                        //     newBoard.PrintTable();
+                        //     Console.WriteLine(5);
+                        // }
+                         states++;
+                         list.Add(newBoard);
+                     }
+                 }
+             }
+             return list;
+         } 
+    }
+}
